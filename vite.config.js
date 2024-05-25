ee8c6c01c788037ec6898f347327b4d73b33decf
Tanmay Gupta <43621750+tanmayGypt@users.noreply.github.com>
2024-05-25 11:08:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Specify the output directory for the built files
-    emptyOutDir: true, // Clear the output directory before building
-    // You may add other build options as needed
-  },
-});
+    outDir: 'dist'  // This is the default, you can change if needed
+  }
+})
